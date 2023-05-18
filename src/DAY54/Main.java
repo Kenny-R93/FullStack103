260561dc2d1098790b733515f513223e2765ba56
Kenny-R93 <krangelunc@gmail.com>
2023-05-18 19:44:33 -0400

    You have the power to inspire and achieve greatness.

---
+++ src/DAY54/Main.java
@@ -0,0 +1,43 @@
+package DAY54;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Test Person class
+
+//        Person Yoda = new Person("Yoda", 800);
+//        System.out.println(Yoda);
+
+        Person Grogu = new Person("Grogu", 54);
+        System.out.println(Grogu);
+
+
+        // Test Calculator class
+
+        // Factorial
+        try {
+            int factorialResult = Calculator.factorial(-1);
+            System.out.println("Factorial calculation: " + factorialResult);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Factorial calculation error: " + e.getMessage());
+        }
+
+
+//            int binomialResult = Calculator.binomialCoefficient(2, 4);
+//            System.out.println(binomialResult);
+
+        // Binomial
+        try {
+            int binomialResult = Calculator.binomialCoefficient(2, 4);
+            System.out.println(binomialResult);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Binomial coefficient calculation error: " + e.getMessage());
+        }
+    }
+
+
+
+    }
+
+
+
