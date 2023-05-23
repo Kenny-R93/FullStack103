20cc30f59e15fc996e1f4fb0bd0d03e9048e582a
Kenny-R93 <krangelunc@gmail.com>
2023-05-22 22:44:47 -0400

    Rise Above

--- src/DAY56/TakingLibraryManagementFurther/Book.java
+++ src/DAY56/TakingLibraryManagementFurther/Book.java
@@ -1,9 +1,54 @@
 package DAY56.TakingLibraryManagementFurther;
 
 public class Book {
-    private String title;
-    private String author;
-    private int publicationYear;
+    private String name;
+    private String content;
+    private int published;
+
     private int pages;
+
+    public Book(String name, int published, int pages, String content) {
+        this.name = name;
+        this.published = published;
+        this.pages = pages;
+        this.content = content;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPublished() {
+        return this.published;
+    }
+
+    public void setPublished(int published) {
+        this.published = published;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String toString() {
+        return "Name: " + this.name + " (" + this.published + ")\n"
+                + "Content: " + this.content;
+    }
 
 }
